592efbe46f4afffed5921d2ff21e13449bb100bf
Fabio Oliveira <obs.fabio.pro@gmail.com>
2023-06-26 13:39:15 -0300

    Created Lofar for python
    separate code for:
          spectrogram with decimation
          logarithmic scale in spectrogram
          lofar calculation
          line normalization between 0 and 1
    
    created lofar test function, validating results based on matlab reference files

--- python/labsonar_sp/analysis.py
+++ python/labsonar_sp/analysis.py
@@ -1,6 +1,11 @@
 import numpy as np
 import scipy.signal as sci
 
+def normalize(x, type=0):
+    if type==0:
+        return (x - np.min(x, axis=0))/(np.max(x, axis=0) - np.min(x, axis=0))
+    raise UnboundLocalError("normalization {:d} not implemented".format(type))
+
 def tpsw(x, npts=None, n=None, p=None, a=None):
     if x.ndim == 1:
         x = x[:, np.newaxis]
@@ -44,3 +49,48 @@ def tpsw(x, npts=None, n=None, p=None, a=None):
     mx[:ix,:]=mx[:ix,:]*(np.matmul(mult,np.ones((1, x.shape[1])))) # Pontos iniciais
     mx[npts-ix:npts,:]=mx[npts-ix:npts,:]*(np.matmul(np.flipud(mult),np.ones((1,x.shape[1])))) # Pontos finais
     return mx
+
+def spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+
+    n_pts = n_pts * 2
+    if n_overlap < 1:
+        n_overlap = np.floor(n_pts * n_overlap)
+    else:
+        n_overlap = n_overlap * 2
+
+    if decimation_rate > 1:
+        dec_data = sci.decimate(data, decimation_rate, 10, 'fir', zero_phase=True)
+        Fs = fs/decimation_rate
+    else:
+        dec_data = data
+        Fs=fs
+
+    freq, time, power = sci.spectrogram(dec_data,
+                                    window=('hann'),
+                                    nperseg=n_pts,
+                                    noverlap=n_overlap,
+                                    nfft=n_pts,
+                                    fs=Fs,
+                                    detrend=False,
+                                    axis=0,
+                                    scaling='spectrum',
+                                    mode='magnitude')
+
+    return power, freq, time
+
+def log_spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+
+    power, freq, time = spectogram(data, fs, n_pts, n_overlap, decimation_rate)
+    power = np.absolute(power)
+    power[power == 0] = 1e-9
+    power = 20*np.log10(power)
+
+    return power, freq, time
+
+def lofar(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+
+    power, freq, time = log_spectogram(data, fs, n_pts, n_overlap, decimation_rate)
+    power = power - tpsw(power)
+    power[power < -0.2] = 0
+
+    return power, freq, time
--- python/test/test_analysis.py
+++ python/test/test_analysis.py
@@ -1,13 +1,11 @@
-import unittest
-import os, json
+import unittest, os, json
 import numpy as np
 import pandas as pd
 import scipy.io.wavfile as wavfile
-import matplotlib.pyplot as plt
+import skimage.io as skimage
 
-from labsonar_sp.analysis import tpsw
+import labsonar_sp.analysis as sp
 
-import os
 
 def find_source_folder(base_folder = None):
     if base_folder is None:
@@ -22,7 +20,6 @@ def find_source_folder(base_folder = None):
             if ret is not None:
                 return ret
         return None
-
 
 class TestAnalysis(unittest.TestCase):
 
@@ -42,11 +39,33 @@ class TestAnalysis(unittest.TestCase):
         if tpsw_alt.ndim == 1:
             tpsw_alt = tpsw_alt[:, np.newaxis]
 
-        py_tpsw_default = tpsw(input)
-        py_tpsw_alt = tpsw(input, args["npts"], args["n"], args["p"], args["a"])
+        py_tpsw_default = sp.tpsw(input)
+        py_tpsw_alt = sp.tpsw(input, args["npts"], args["n"], args["p"], args["a"])
 
-        defaul_diff = np.count_nonzero(np.subtract(py_tpsw_default,tpsw_default) > 1)
+        default_diff = np.count_nonzero(np.subtract(py_tpsw_default,tpsw_default) > 1)
         alt_diff = np.count_nonzero(np.subtract(py_tpsw_alt,tpsw_alt) > 1)
 
-        self.assertTrue(defaul_diff == 0)
+        self.assertTrue(default_diff == 0)
+        self.assertTrue(alt_diff == 0)
+
+    def test_lofar(self):
+        source_folder=find_source_folder()
+
+        with open(os.path.join(source_folder,"config.json"), 'r') as f:
+            test_config = json.load(f)
+
+        fs, input = wavfile.read(os.path.join(source_folder, test_config["input"]))
+        lofar_default = skimage.imread(os.path.join(source_folder, test_config['lofar']['default']))
+        args = test_config['lofar']['args']
+        lofar_alt = skimage.imread(os.path.join(source_folder, args['filename']))
+
+        py_lofar_default, _, _ = sp.lofar(input[:,1], fs)
+        py_lofar_default = sp.normalize(py_lofar_default).T
+        py_lofar_alt, _, _ = sp.lofar(input[:,1], fs, args["npts"], args["novr"], args["decimation"])
+        py_lofar_alt = sp.normalize(py_lofar_alt).T
+
+        default_diff = np.count_nonzero(np.subtract(py_lofar_default, lofar_default) > 1)
+        alt_diff = np.count_nonzero(np.subtract(py_lofar_alt, lofar_alt) > 1)
+
+        self.assertTrue(default_diff == 0)
         self.assertTrue(alt_diff == 0)
