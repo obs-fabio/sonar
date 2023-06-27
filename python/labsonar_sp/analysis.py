6e32dfaaaaa7017ad1a49854cff8ab19dcf9200c
Fabio Oliveira <obs.fabio.pro@gmail.com>
2023-06-27 00:20:08 -0300

    Fixes in python validation
          Fixed comparison thresholds previous margin too large,
    had significant errors being ignored

--- python/labsonar_sp/analysis.py
+++ python/labsonar_sp/analysis.py
@@ -1,9 +1,12 @@
 import numpy as np
 import scipy.signal as sci
+import librosa
 
 def normalize(x, type=0):
-    if type==0:
+    if type == 0: # normalize between 0 and 1
         return (x - np.min(x, axis=0))/(np.max(x, axis=0) - np.min(x, axis=0))
+    if type == 1: # normalize -1 e 1, keeping 0 in place (librosa.util.normalize)
+        return x/np.max(np.abs(x), axis=0)
     raise UnboundLocalError("normalization {:d} not implemented".format(type))
 
 def tpsw(x, npts=None, n=None, p=None, a=None):
@@ -20,7 +23,7 @@ def tpsw(x, npts=None, n=None, p=None, a=None):
     if a is None:
         a = 2.0
     if p>0:
-        h = np.concatenate((np.ones((n-p+1)), np.zeros(2 * p-1), np.ones((n-p+1))), axis=None)
+        h = np.concatenate((np.ones((n-p+1)), np.zeros(2 * p-1), np.ones((n-p+1))))
     else:
         h = np.ones((1, 2*n+1))
         p = 1
@@ -28,29 +31,27 @@ def tpsw(x, npts=None, n=None, p=None, a=None):
     h /= np.linalg.norm(h, 1)
     def apply_on_spectre(xs):
         return sci.convolve(h, xs, mode='full')
-
     mx = np.apply_along_axis(apply_on_spectre, arr=x, axis=0)
-    ix = int(np.floor((h.shape[0] + 1)/2.0)) # Defasagem do filtro
-    mx = mx[ix-1:npts+ix-1] # Corrige da defasagem
-    # Corrige os pontos extremos do espectro
-    ixp = ix - p
-    mult=2*ixp/np.concatenate([np.ones(p-1)*ixp, range(ixp,2*ixp + 1)], axis=0)[:, np.newaxis] # Correcao dos pontos extremos
-    mx[:ix,:] = mx[:ix,:]*(np.matmul(mult, np.ones((1, x.shape[1])))) # Pontos iniciais
-    mx[npts-ix:npts,:]=mx[npts-ix:npts,:]*np.matmul(np.flipud(mult),np.ones((1, x.shape[1]))) # Pontos finais
 
-    # Elimina picos para a segunda etapa da filtragem
-    #indl= np.where((x-a*mx) > 0) # Pontos maiores que a*mx
+    ix = int(np.floor((h.shape[0] + 1)/2.0))
+    mx = mx[ix-1:npts+ix-1]
+    ixp = ix - p
+    mult=2*ixp/np.concatenate([np.ones(p-1)*ixp, range(ixp,2*ixp + 1)], axis=0)[:, np.newaxis]
+    mx[:ix,:] = mx[:ix,:]*(np.matmul(mult, np.ones((1, x.shape[1]))))
+    mx[npts-ix:npts,:]=mx[npts-ix:npts,:]*np.matmul(np.flipud(mult),np.ones((1, x.shape[1])))
+
     indl = (x-a*mx) > 0
-    #x[indl] = mx[indl]
     x = np.where(indl, mx, x)
     mx = np.apply_along_axis(apply_on_spectre, arr=x, axis=0)
+    # mx = sci.convolve2d(h, x, mode='valid')
     mx=mx[ix-1:npts+ix-1,:]
-    #Corrige pontos extremos do espectro
-    mx[:ix,:]=mx[:ix,:]*(np.matmul(mult,np.ones((1, x.shape[1])))) # Pontos iniciais
-    mx[npts-ix:npts,:]=mx[npts-ix:npts,:]*(np.matmul(np.flipud(mult),np.ones((1,x.shape[1])))) # Pontos finais
+    mx[:ix,:]=mx[:ix,:]*(np.matmul(mult,np.ones((1, x.shape[1]))))
+    mx[npts-ix:npts,:]=mx[npts-ix:npts,:]*(np.matmul(np.flipud(mult),np.ones((1,x.shape[1]))))
     return mx
 
-def spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+def spectrogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+
+    data = data - np.mean(data)
 
     n_pts = n_pts * 2
     if n_overlap < 1:
@@ -59,29 +60,27 @@ def spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
         n_overlap = n_overlap * 2
 
     if decimation_rate > 1:
-        dec_data = sci.decimate(data, decimation_rate, 10, 'fir', zero_phase=True)
+        data = sci.decimate(data, decimation_rate)
         Fs = fs/decimation_rate
     else:
-        dec_data = data
+        data = data
         Fs=fs
 
-    freq, time, power = sci.spectrogram(dec_data,
-                                    window=('hann'),
-                                    nperseg=n_pts,
-                                    noverlap=n_overlap,
+    freq, time, power = sci.spectrogram(data,
                                     nfft=n_pts,
                                     fs=Fs,
+                                    window=np.hanning(n_pts),
+                                    noverlap=n_overlap,
                                     detrend=False,
-                                    axis=0,
                                     scaling='spectrum',
-                                    mode='magnitude')
-
+                                    mode='complex')
+    power = np.abs(power)*n_pts/2
     return power, freq, time
 
-def log_spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
+def log_spectrogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
 
-    power, freq, time = spectogram(data, fs, n_pts, n_overlap, decimation_rate)
-    power = np.absolute(power)
+    power, freq, time = spectrogram(data, fs, n_pts, n_overlap, decimation_rate)
+    aux = power
     power[power == 0] = 1e-9
     power = 20*np.log10(power)
 
@@ -89,8 +88,8 @@ def log_spectogram(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
 
 def lofar(data, fs, n_pts=1024, n_overlap=0, decimation_rate=1):
 
-    power, freq, time = log_spectogram(data, fs, n_pts, n_overlap, decimation_rate)
+    power, freq, time = log_spectrogram(data, fs, n_pts, n_overlap, decimation_rate)
     power = power - tpsw(power)
+    aux = power
     power[power < -0.2] = 0
-
     return power, freq, time
