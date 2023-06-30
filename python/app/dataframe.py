abe86f8a5fb43b3ce172229284ab1cebf2f0ad5f
Fabio Oliveira <obs.fabio.pro@gmail.com>
2023-06-30 12:29:43 -0300

    App created to run analyzes
         application created to analyze datasets
         increasing generalization of create dataframe
         saving dataframe as binary

--- python/app/dataframe.py
+++ python/app/dataframe.py
@@ -17,19 +17,20 @@ decimation_rate=4
 
 input_dir = "/home/sonar/Data/4classes"
 
-# def get_class(abs_filename:str) -> str:
-#     path, rel_filename = os.path.split(abs_filename)
-#     filename, extension = os.path.splitext(rel_filename)
-#     return filename.split('_')[0]
+def get_class(abs_filename:str) -> str:
+    path, rel_filename = os.path.split(abs_filename)
+    filename, extension = os.path.splitext(rel_filename)
+    return filename.split('_')[0]
 
 analyzes = [sp.Analysis.LOG_SPECTROGRAM, sp.Analysis.LOFAR, sp.Analysis.MELGRAM]
 for analysis in tqdm(analyzes, desc='DataFrames'):
     sp_df.create_window_dataframe(analysis,
             input_dir,
-            input_dir + "/df_{:s}.csv".format(str(analysis)),
+            input_dir + "/df_{:s}.pkl".format(str(analysis)),
             # extract_class=get_class,
             n_pts=n_pts,
             n_overlap=n_overlap,
             n_mels=n_mels,
             decimation_rate=decimation_rate
         )
+    
--- python/labsonar_sp/dataframe.py
+++ python/labsonar_sp/dataframe.py
@@ -6,6 +6,8 @@ from typing import Callable
 import tifffile
 import random
 import string
+import time
+from typing import List
 
 
 import labsonar_sp.analysis as sp
@@ -20,6 +22,32 @@ def get_class(filename:str) -> str:
 
 def get_file_id(filename:str) -> str:
     return filename
+
+def create_window_dataframe_from_tiff_files(
+                            file_map_list: List[str],
+                            output_filename: str,
+                            extract_class: Callable[[str], str] = get_class,
+                            extract_file_id: Callable[[str], str] = get_file_id):
+
+    dfs = []
+    for file_map in tqdm(file_map_list, desc='Files', leave=False):
+        class_id = extract_class(file_map['input'])
+        file_id = extract_file_id(file_map['input'])
+
+        data = tifffile.imread(file_map['output'])
+        data = data.T
+
+        df_file = pd.DataFrame(data, columns=[f"feature_{i}" for i in range(data.shape[1])])
+        df_file["file_id"] = file_id
+        df_file["class_id"] = class_id
+
+        dfs.append(df_file)
+
+    df = pd.concat(dfs, ignore_index=True)
+    if output_filename.endswith(".csv"):
+        df.to_csv(output_filename, index=False)
+    else:
+        df.to_pickle(output_filename)
 
 def create_window_dataframe(analysis: sp.Analysis,
                             input_dir: str,
@@ -41,19 +69,9 @@ def create_window_dataframe(analysis: sp.Analysis,
 
     file_map_list = manager.eval_dir(input_dir=input_dir, output_dir=output_dir, extension=extension)
 
-    dfs = []
-    for file_map in tqdm(file_map_list, desc='Files', leave=False):
-        class_id = extract_class(file_map['input'])
-        file_id = extract_file_id(file_map['input'])
-
-        data = tifffile.imread(file_map['output'])
-        data = data.T
-
-        df_file = pd.DataFrame(data, columns=[f"feature_{i}" for i in range(data.shape[1])])
-        df_file["file_id"] = file_id
-        df_file["class_id"] = class_id
-
-        dfs.append(df_file)
-
-    df = pd.concat(dfs, ignore_index=True)
-    df.to_csv(output_filename, index=False)
+    create_window_dataframe_from_tiff_files(
+        file_map_list = file_map_list,
+        output_filename = output_filename,
+        extract_class = extract_class,
+        extract_file_id = extract_file_id
+    )
